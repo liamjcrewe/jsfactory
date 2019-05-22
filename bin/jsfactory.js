2eec06ef6e278a4a95df9dc6d4e18f384fddb643
Liam Crewe <liam.j.crewe@gmail.com>
2019-05-22 20:27:10 +0100

    Gather required inputs using inquirer

--- bin/jsfactory.js
+++ bin/jsfactory.js
@@ -1,8 +1,37 @@
 #! /usr/bin/env node
 const program = require('commander')
+const inquirer = require('inquirer')
 
-const run = () => {
+const gatherInputs = () =>
+  inquirer.prompt([
+    {
+      name: 'name',
+      message: 'Name: '
+    },
+    {
+      name: 'description',
+      message: 'Description: ',
+      default: ''
+    },
+    {
+      name: 'author',
+      message: 'Author: ',
+      default: ''
+    },
+    {
+      name: 'license',
+      message: 'License: ',
+      default: 'MIT'
+    }
+  ])
+
+const run = async () => {
   console.log('Running...')
+
+  console.log('Gathering inputs...')
+  const { name, description, author, license } = await gatherInputs()
+
+  console.log(name, description, author, license)
 }
 
 program.version('0.0.1').parse(process.argv)
